651b5ecd53c9ffdf867a2f8734d8e5ddfa483bfb
Shristi Chapagain <schapaga@cord.edu>
2022-10-08 18:54:08 -0400

    First Commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+	<title>Food Connect</title>
+    <link rel="stylesheet" href="styles/style.css" media="all"/>
+</head>
+<body>
+    <?php
+    include("header.php");
+    ?>
+
+<div class="main_content main_gradient">
+    <div class="content">
+        <p id="main_content_title">Lets share food</p>
+        <p></p>
+        <p id="main_content_desc">
+            Lets share food (description of the project)! Lets share food (description of the project)! Lets share food (description of the project)!
+        </p>
+       
+        
+            <a href=""> <button class="button" id="">Give-Food</button></a>
+            <a href=""> <button class="button" id="">Get-Food</button></a>
+       
+
+        
+    </div>
+    <div class="content_2">
+        <img id="food_1" src="img/groceries.jpeg"/>
+    </div>
+</div>
+
+<?php
+    include("footer.php");
+?>
+
+
+
+</body>
+</html>
